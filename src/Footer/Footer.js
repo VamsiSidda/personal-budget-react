0340512b51495831ac2eb8490f04dfe64c4e7092
VamsiSidda <vamsisidda777@gmail.com>
2023-09-30 16:51:17 -0400

    Brought PbBudget Code In

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer class="bottom" role="contentinfo">
+        <div class="center">
+            All rights reserved &copy; Vamsi Sidda
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
